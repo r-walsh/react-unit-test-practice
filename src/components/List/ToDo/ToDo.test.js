f43790e796c334a60b7dd90ac211719358c92821
Ryan Walsh <ryan@thehatrack.net>
2017-08-09 13:29:17 -0600

    remove sinon, add snapshots

--- src/components/List/ToDo/ToDo.test.js
+++ src/components/List/ToDo/ToDo.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import toJson from 'enzyme-to-json';
 import { shallow } from 'enzyme';
-import sinon from 'sinon';
 
 import ToDo from './ToDo';
 
 describe('ToDo', () => {
   // Using jest to set up a single unit test.
-  // First argument is just an explanation of what the test is.
-  // This is what will display on failing tests, so make it descriptive.
+  // The first argument is just an explanation of what the test is,
+  // this is what will be output while tests run, so make it descriptive.
   it('displays text based on props.text', () => {
     // Creating a shallow rendered(pared down) version of the component with Enzyme.
     const toDo = shallow(
@@ -37,35 +37,27 @@ describe('ToDo', () => {
         toggleCompletion={() => null}
       />,
     );
+    const inCompleteToDo = shallow(
+      <ToDo
+        complete={false}
+        deleteToDo={() => null}
+        text="Test ToDo"
+        toggleCompletion={() => null}
+      />,
+    );
 
-    // Here we find all elements with a class of ".to-do__info--complete
-    // Because there is only one within the component it should be an array of length 1
-    expect(toDo.find('.to-do__info--complete')).toHaveLength(1);
-    // We also want to check the alternate class the element could have,
-    // and make sure that wasn't rendered as well for some reason.
-    expect(toDo.find('.to-do__info')).toHaveLength(0);
-
-    // It is common to need to test changes in props or state.
-    // This is done via component.setProps( { /* values */ } ) or component.setState( { /* values */ } )
-    toDo.setProps({
-      complete: false,
-      deleteToDo() {
-        return null;
-      },
-      text: 'Test ToDo',
-      toggleCompletion() {
-        return null;
-      },
-    });
-
-    expect(toDo.find('.to-do__info--complete')).toHaveLength(0);
-    expect(toDo.find('.to-do__info')).toHaveLength(1);
+    // The first time this test is run, it will generate a snapshot of the component's output.
+    // As further changes are made to the component, you simply review the diff to ensure
+    // that the changes are intentional.
+    expect(toJson(toDo)).toMatchSnapshot();
+    // We still need to check both potential states of the component!
+    expect(toJson(inCompleteToDo)).toMatchSnapshot();
   });
 
   it('ToDo calls props.toggleCompletion on checkbox change', () => {
     // Creating a sinon spy,
     // essentially a dummy function who's whole purpose is to tell us information about how it is called.
-    const toggleCompletionSpy = sinon.spy();
+    const toggleCompletionSpy = jest.fn();
 
     // We pass the spy as a prop to the component
     const toDo = shallow(
@@ -83,13 +75,14 @@ describe('ToDo', () => {
     // matching the event handler inside the component.
     toDo.find('.to-do__completion').simulate('change');
 
-    // We use a sinon assertion to confirm that our callback spy was called as many times as expected.
-    // We could also check args with toggleCompletionSpy.firstCall.args
-    sinon.assert.calledOnce(toggleCompletionSpy);
+    // Here we check whether our mocked function has been called the expected number of times
+    // If we needed to access the arguments we could say
+    // expect(toggleCompletionSpy.mock.calls[0]).toEqual(expectedArguments);
+    expect(toggleCompletionSpy.mock.calls).toHaveLength(1);
   });
 
   it('ToDo calls props.deleteTodo on delete button click', () => {
-    const deleteToDoSpy = sinon.spy();
+    const deleteToDoSpy = jest.fn();
 
     const toDo = shallow(
       <ToDo
@@ -102,6 +95,6 @@ describe('ToDo', () => {
 
     toDo.find('button').simulate('click');
 
-    sinon.assert.calledOnce(deleteToDoSpy);
+    expect(deleteToDoSpy.mock.calls).toHaveLength(1);
   });
 });
--- test-config/index.js
+++ test-config/index.js
@@ -1,5 +1,4 @@
 process.env.NODE_ENV = 'test';
-process.env.PUBLIC_URL = '';
 
 const jest = require('jest');
 const argv = process.argv.slice(2);
