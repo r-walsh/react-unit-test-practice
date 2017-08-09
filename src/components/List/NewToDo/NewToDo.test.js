44a6d1a3ebeef44b78e175d7fd0195b8050e7921
Ryan Walsh <ryan@thehatrack.net>
2017-08-09 14:22:44 -0600

    add tests for solution

--- src/components/List/NewToDo/NewToDo.test.js
+++ src/components/List/NewToDo/NewToDo.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import toJson from 'enzyme-to-json';
 import { shallow } from 'enzyme';
 
 import NewToDo from './NewToDo';
@@ -7,6 +8,25 @@ describe('NewToDo', () => {
   it('renders', () => {
     const newToDo = shallow(<NewToDo submit={() => {}} />);
 
-    expect(newToDo).toBeTruthy();
+    expect(toJson(newToDo)).toMatchSnapshot()
   });
+
+  it('updates state on input change', () => {
+    const newToDo = shallow(<NewToDo submit={() => {}}/>)
+
+    newToDo.find('.new-to-do__input').simulate('change', { target: { value: 'Give a talk on unit testing!'}});
+
+    expect(newToDo.state().toDo).toBe('Give a talk on unit testing!')
+  })
+
+  it('calls props.submit on submit', () => {
+    const submit = jest.fn();
+    const newToDo = shallow(<NewToDo submit={submit}/>)
+
+    newToDo.setState({ toDo: 'Hit 100% coverage' });
+    newToDo.find('.new-to-do').simulate('submit', { preventDefault() {} });
+
+    expect(submit.mock.calls).toHaveLength(1);
+    expect(submit.mock.calls[0]).toEqual(['Hit 100% coverage']);
+  })
 });
