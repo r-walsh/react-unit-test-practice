e50fcbbf621d6e48080e5fd1d6da9377d36c799e
Ryan Walsh <ryan@thehatrack.net>
2017-08-16 09:49:13 -0500

    remove lots of unneeded stuffs

--- test-config/index.js
+++
@@ -1,6 +0,0 @@
-process.env.NODE_ENV = 'test';
-
-const jest = require('jest');
-const argv = process.argv.slice(2);
-
-jest.run(argv);
--- test-config/transform.js
+++
@@ -1,4 +0,0 @@
-const rc = require('../package.json').babel;
-const babelJest = require('babel-jest');
-
-module.exports = babelJest.createTransformer(rc);
